c299df592324c7543d466a96506eec900b2aacac
AYUSHDUBEY24 <mrakdubey2004@gmail.com>
2026-04-16 21:40:26 +0530

    added monitoring script

---
+++ monitor.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime
+
+
+URL ="https://ayushdubey24.github.io/devops/"
+
+def check_website():
+    try:
+        response = requests.get(URL, timeout=5)
+        print("Status Code:", response.status_code)
+
+        if response.status_code == 200:
+            return "UP"
+        else:
+            return "DOWN"
+
+    except Exception as e:
+        print("Error:", e)
+        return "DOWN"
+
+def log_status(status):
+    with open("log.txt", "a") as file:
+        file.write(f"{datetime.now()} - {status}\n")
+
+# 👉 Main execution
+status = check_website()
+log_status(status)
+
+print(f"Final Result: Website is {status}")
